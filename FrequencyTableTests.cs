1d910a0d76cdf91c636674f95c5a8375331425f4
Steve Colonna <quarterback16@live.com.au>
2025-07-15 16:38:37 +1000

    Tidy up Frequency Table Markdown

---
+++ FrequencyTableTests.cs
@@ -0,0 +1,23 @@
+﻿namespace RosterLib.Tests
+{
+    [TestClass]
+    public class FrequencyTableTests
+    {
+        #region  Sut Initialisation
+
+        private FrequencyTable sut;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            sut = SystemUnderTest();
+        }
+
+        private static FrequencyTable SystemUnderTest()
+        {
+            return new FrequencyTable("TestFrequencyTable");
+        }
+
+        #endregion
+    }
+}
---
+++ WizSeasonTests.cs
@@ -0,0 +1,50 @@
+﻿namespace RosterLib.Tests
+{
+    [TestClass]
+    public class WizSeasonTests
+    {
+        #region  Sut Initialisation
+
+        private WizSeason sut;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            sut = SystemUnderTest();
+        }
+
+        private static WizSeason SystemUnderTest() =>
+            new WizSeason(
+                "2022",
+                process: false);
+
+        #endregion
+
+        [TestMethod]
+        public void WizSeasonInstantiatesOk()
+        {
+            Assert.IsNotNull(sut);
+        }
+
+        [TestMethod]
+        public void TestCurrentSeason()
+        {
+            sut.LoadSeason(
+                seasonIn: "2024",
+                startWeekIn: "01",
+                endWeekIn: "18");
+
+            sut.OutputPhase();
+        }
+
+        [TestMethod]
+        public void WizSeason_HasFrequencyTables()
+        {
+            sut.LoadSeason(
+                startWeekIn: "01",
+                endWeekIn: "18");
+
+            sut.FrequencyTables(toMarkdown: true);
+        }
+    }
+}
