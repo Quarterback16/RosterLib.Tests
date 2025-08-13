0257f31ad0cd2aa3954a9b45cc6ca1d3313a1aa9
Steve Colonna <quarterback16@live.com.au>
2025-08-13 15:15:56 +1000

    Fix ADP file name

---
+++ HotListTests.cs
@@ -0,0 +1,29 @@
+﻿namespace RosterLib.Tests
+{
+	[TestClass]
+	public class HotListTests
+	{
+		[TestMethod]
+		public void TestDoHotlistsJob()  //  2015-11-08  1 min
+		{
+			var sut = new HotListReporter(
+			new TimeKeeper(null));
+			sut.Configs.Clear();
+			sut.Configs.Add(new HotListConfig
+			{
+				Category = "3",
+				Position = "WR",
+				FreeAgents = true,
+				Starters = true
+			});
+
+			var result = sut.DoReport();
+
+			Assert.IsFalse(
+				string.IsNullOrEmpty(
+					result));
+
+		}
+
+	}
+}
