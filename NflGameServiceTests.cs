5b2857c86881bfef6e57fcf43d0a3b73e757bc3f
Steve Colonna <quarterback16@live.com.au>
2025-08-16 09:58:49 +1000

    Add Game Service tests and new constructor

---
+++ NflGameServiceTests.cs
@@ -0,0 +1,25 @@
+﻿using RosterLib.Implementations;
+
+namespace RosterLib.Tests
+{
+	[TestClass]
+	public class NflGameServiceTests
+	{
+		private NflGameService _sut;
+
+		[TestInitialize]
+		public void Setup()
+		{
+			_sut = new NflGameService(2024);
+		}
+
+		[TestMethod]
+		public void NflGameService_CanLoadGames()
+		{
+			var games = _sut.GetAll();
+			Assert.IsNotNull(games, "Games should not be null");
+			Assert.IsTrue(games.Count > 0, "Games list should not be empty");
+			Console.WriteLine($"Games Loaded: {games.Count}");
+		}
+	}
+}
