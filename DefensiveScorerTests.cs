8d70641607e4c7a7c58864b3a2f987023db055f6
Steve Colonna <quarterback16@live.com.au>
2025-09-01 15:02:49 +1000

    Send Defensive reports to Markdown

---
+++ DefensiveScorerTests.cs
@@ -0,0 +1,37 @@
+﻿namespace RosterLib.Tests
+{
+    [TestClass]
+    public class DefensiveScorerTests
+    {
+        #region  Sut Initialisation
+
+        private DefensiveScorer sut;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            sut = SystemUnderTest();
+        }
+
+        private static DefensiveScorer SystemUnderTest() =>
+            new DefensiveScorer(
+                new TimeKeeper(clock:null));
+
+        #endregion
+
+        [TestMethod]
+        public void DefensiveScorerInstantiatesOk()
+        {
+            Assert.IsNotNull(sut);
+        }
+
+        [TestMethod]
+        public void DefensiveScorerExecutesOk()
+        {
+            var result = sut.DoReport();
+            Assert.IsNotNull(result);
+        }
+
+    }
+}
+
