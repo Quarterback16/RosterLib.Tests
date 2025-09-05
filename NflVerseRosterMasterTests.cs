3c6217684d0d25cce6b50e3495f2074880997498
Steve Colonna <quarterback16@live.com.au>
2025-09-05 17:20:13 +1000

    start the nflverse roster service

---
+++ NflVerseRosterMasterTests.cs
@@ -0,0 +1,23 @@
+﻿using RosterLib.Services;
+
+namespace RosterLib.Tests
+{
+	[TestClass]
+	public class NflVerseRosterMasterTests
+	{
+		private NflVerseRosterMaster _rosterMaster;
+
+		[TestInitialize]
+		public void Setup()
+		{
+			_rosterMaster = new NflVerseRosterMaster(
+				"2025");
+		}
+
+		[TestMethod]
+		public void ServiceInstantiatesOk()
+		{
+			Assert.IsNotNull(_rosterMaster);
+		}
+	}
+}
