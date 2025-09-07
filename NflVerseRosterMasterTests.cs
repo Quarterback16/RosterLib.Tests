3f2f70b65e601473d4ad91e7dd790d90bf7f76f7
Steve Colonna <quarterback16@live.com.au>
2025-09-07 17:47:59 +1000

    Nfl verse roster csv

--- NflVerseRosterMasterTests.cs
+++ NflVerseRosterMasterTests.cs
@@ -1,4 +1,6 @@
-﻿using RosterLib.Services;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RosterLib.Models;
+using RosterLib.Services;
 
 namespace RosterLib.Tests
 {
@@ -18,6 +20,78 @@ namespace RosterLib.Tests
 		public void ServiceInstantiatesOk()
 		{
 			Assert.IsNotNull(_rosterMaster);
+		}
+
+		[TestMethod]
+		public void ServiceCanGetDepthChart()
+		{
+			var teamCode = "SF";
+			var result = _rosterMaster.GetDepthChart(
+				teamCode);
+			Assert.IsNotNull(result);
+			Assert.IsTrue(result.PlayersOnRoster > 40);
+			Assert.IsTrue(
+				result.PlayersOnRoster < 70,
+				$@"roster has too many players {
+					result.PlayersOnRoster
+					}");
+			Console.WriteLine(
+				$@"{
+					teamCode
+					} roster has {
+					result.PlayersOnRoster
+					} players");
+		}
+
+		[TestMethod]
+		public void ServiceKnowsSfQb()
+		{
+			var result = _rosterMaster.PlayerAt(
+				"SF",
+				"QB",
+				1);
+
+			Assert.AreEqual(
+				"Brock Purdy",
+				result);
+		}
+
+		[TestMethod]
+		public void ServiceKnowsSfQbPlayerInfo()
+		{
+			var result = _rosterMaster.PlayerInfoAt(
+				"SF",
+				"QB",
+				1);
+
+			Assert.IsInstanceOfType<PlayerInfo?>(
+				result);
+
+			Console.WriteLine(
+				result.BirthDate);
+			Console.WriteLine(
+				result.College);
+		}
+
+		[TestMethod]
+		public void ServiceKnowsSfQbPlayerInfoAt()
+		{
+			var result = _rosterMaster.PlayerInfoAt(
+				"SF",
+				"QB",
+				2);
+
+			Assert.IsInstanceOfType<PlayerInfo?>(
+				result);
+
+			Assert.AreEqual(
+				"Mac Jones",
+				result.FullName);
+
+			Console.WriteLine(
+				result.BirthDate);
+			Console.WriteLine(
+				result.College);
 		}
 	}
 }
