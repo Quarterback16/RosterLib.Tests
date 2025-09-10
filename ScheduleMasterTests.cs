e6d977d8ba1173795ba11c2f0956cea9a9d0f45c
Steve Colonna <quarterback16@live.com.au>
2025-09-11 09:10:31 +1000

    add another test

--- ScheduleMasterTests.cs
+++ ScheduleMasterTests.cs
@@ -50,6 +50,21 @@ namespace RosterLib.Tests
 		}
 
 		[TestMethod]
+		public void ScheduleMaster_KnowsYahooOpponent_of77_2025_02()
+		{
+			var opponent = Sut.OpponentOf(
+				team: "7x7ers",
+				leagueCode: "YAH",
+				season: 2025,
+				round: 02);
+			Assert.IsNotNull(opponent);
+			Assert.AreEqual("Super Hawks", opponent);
+			Assert.AreEqual("SH", CodeHelper.CodeFor("YAH", opponent));
+
+			Console.WriteLine(opponent);
+		}
+
+		[TestMethod]
 		public void ScheduleMaster_HandlesNoGame_Ok()
 		{
 			var game = Sut.GetGame(
