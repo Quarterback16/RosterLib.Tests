fb1009ed37db31bf07d04d460a2a5d73139904fb
Steve Colonna <quarterback16@live.com.au>
2025-09-28 12:27:35 +1000

    WKI-2025-055-Improve Run Report

---
+++ RunReportTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace RosterLib.Tests
+{
+    [TestClass]
+    public class RunReportTests
+    {
+        [TestMethod]
+        public void RunReport_RendersHtml()
+        {
+            var rr = new RunReport(timekeeper: new TimeKeeper(clock:null))
+            {
+                NoOfDaysBack = 7
+            };
+            rr.RenderAsHtml();
+            var fileOut = rr.OutputFilename();
+            Assert.IsTrue(
+                File.Exists(fileOut),
+                $"Cannot find {fileOut}");
+        }
+    }
+}
