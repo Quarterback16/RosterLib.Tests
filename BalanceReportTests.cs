43fef96e6e2b1aa236d35c7ad3f638f2a3bf0857
Steve Colonna <quarterback16@live.com.au>
2026-05-05 09:51:03 +1000

    fix grey background

---
+++ BalanceReportTests.cs
@@ -0,0 +1,25 @@
+﻿namespace RosterLib.Tests
+{
+    [TestClass]
+    public class BalanceReportTests
+    {
+        [TestMethod]
+        public void TestBalanceReportToMarkdown()
+        {
+            var tk = new FakeTimeKeeper("2025");
+            var sut = new BalanceReport(tk);
+            Assert.IsNotNull(sut);
+            sut.RenderToMarkDown_TDR();
+        }
+
+        [TestMethod]
+        public void TestDoBalanceReportJobAnyYear()
+        {
+            var tk = new FakeTimeKeeper("2026");
+            var sut = new BalanceReport(tk);
+            Assert.IsNotNull(sut);
+            sut.Render(mdOnly: true);
+        }
+
+    }
+}
