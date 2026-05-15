2b2f834cbb6ab7a602a26092c4db0e5789be9030
Steve Colonna <quarterback16@live.com.au>
2026-05-16 08:07:11 +1000

    Fix null bug

---
+++ FaMarketTests.cs
@@ -0,0 +1,17 @@
+﻿namespace RosterLib.Tests
+{
+    [TestClass]
+    public class FaMarketTests
+    {
+        [TestMethod]
+        public void TestFaMarket()
+        {
+            var timekeeper = new TimeKeeper(null);
+            var report = new FaMarket(timekeeper);
+            report.DoReport();
+            var teams = report.GetTeams();
+            Assert.IsNotNull(teams);
+            Assert.IsTrue(teams.Rows.Count > 0);
+        }
+    }
+}
